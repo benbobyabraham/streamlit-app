a5e4e75175c4747a490184310deebdef3c93f709
Ben Boby <47517645+benbobyabraham@users.noreply.github.com>
2021-06-10 04:38:33 +0530

    Update main_prog.py

--- main_prog.py
+++ main_prog.py
@@ -2,27 +2,27 @@
 
 from numpy.lib.function_base import select
 import streamlit as st
-import grayscale
-import bgr
-import blur
-import Edge_detection
-import R_B_G_channel
-import thresholding_image
-import erosion
-import dilation
-import Grayscale_inverse
+#import grayscale
+#import bgr
+#import blur
+#import Edge_detection
+#import R_B_G_channel
+#import thresholding_image
+#import erosion
+#import dilation
+#import Grayscale_inverse
 
-pages = {
-    "R-G-B-channels" : R_B_G_channel,
-    "Grayscale" : grayscale,
-    "Grayscale Inverse" : Grayscale_inverse,
-    "BGR scale" : bgr,
-    "Blurring" : blur,
-    "Thresholding" : thresholding_image,
-    "Edge detection" : Edge_detection,
-    "Erosion" : erosion,
-    "Dilation" : dilation
-}
+#pages = {
+#    "R-G-B-channels" : R_B_G_channel,
+#    "Grayscale" : grayscale,
+#    "Grayscale Inverse" : Grayscale_inverse,
+#    "BGR scale" : bgr,
+#    "Blurring" : blur,
+#    "Thresholding" : thresholding_image,
+#    "Edge detection" : Edge_detection,
+#    "Erosion" : erosion,
+#    "Dilation" : dilation
+#}
 
 st.sidebar.title('NAVIGATION')
 select = st.sidebar.selectbox("Go To", ["Image" , "Video"])
